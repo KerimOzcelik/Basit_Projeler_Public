90c94b2f9ff1af7cbb691315d64d20a6b1bbb7da
Kerim Özçelik <kermozcelk@gmail.com>
2024-10-15 19:28:08 +0300

    Add files via upload

---
+++ Hesap_Makinesi/Hesap_Makinesi/Form1.cs
@@ -0,0 +1,62 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hesap_Makinesi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sayi1, sayi2;
+           
+            sayi1 = Convert.ToInt32(textBox1.Text);
+            sayi2 = Convert.ToInt32(textBox2.Text);
+            string islem = textBox3.Text;
+            double sonuc = 0;
+
+            switch (islem)
+            {
+                case "+": sonuc = sayi1 + sayi2; break;
+                case "-": sonuc = sayi1 - sayi2; break;
+                case "*": sonuc= sayi1 * sayi2; break;
+                case "/": 
+                    if (sayi2 != 0)
+                    {
+                        sonuc = sayi1 / sayi2;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Paydadaki sayı 0 olamaz.");
+                       return;
+                    }
+                    break;
+
+          
+                    default:
+                    MessageBox.Show("Geçersiz operatör.");
+                    return ;
+            }
+
+            label5.Text = "" + sonuc.ToString();
+        }
+    }
+}
